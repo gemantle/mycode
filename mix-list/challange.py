d1a89fbc2609878f6aff22984636b4ffb4a677d8
Glenn Mantle <glenn.e.mantle@verizon.com>
2019-06-10 15:42:58 -0400

    update

---
+++ mix-list/challange.py
@@ -0,0 +1,4 @@
+dream_cars = {"car_1": "bugatti", "car_2": "ferarri"}
+boats = ["Pinta", "Nina", "Santa-Maria"]
+
+print("The " + dream_cars['car_1'] + " was on the " + boats[2] + " and the " + dream_cars['car_2'] + boats[1]) 
---
+++ mix-list/mix_list.py
@@ -0,0 +1,8 @@
+my_list = [ "192.168.0.5", 5060, "UP" ]
+print("The frist item in the list (IP): " +my_list[0] )
+print("The second item in the list is the (port):" +str(my_list[1]) )
+print("The last item in the list is (state): " +my_list[2] )
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("When I " +new_list[5], "into the IP addresses " +new_list[3], " or " +new_list[4], " I am unable to ping ports " +new_list[0], ","  )
+
+
